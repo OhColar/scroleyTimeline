9e83098e139afe52f82cce5834115d1e0170ff7b
OhColar <alex@arcdevelop.co.uk>
2022-12-06 10:26:37 +0000

    add drag

--- src/js/app.js
+++ src/js/app.js
@@ -54,4 +54,57 @@ timeline.addEventListener("scroll", event => {
     //timelineTrigger(year)
 }, { passive: true });
 
+// Scroll hijack
+// =======================================
+// timeline.addEventListener('wheel', (ev) => {
+//     // Get scroll progress 0 - 1
+//     let timelineWidth = timeline.clientWidth,
+//         bodyWidth =  timeline.scrollWidth - timelineWidth,
+//         scrollPos = timeline.scrollLeft
+//     const calc = ((scrollPos/bodyWidth)*100)/100;
+//
+//     // Check to see if the direction is not UP and the progress is less than 1
+//
+//     if(calc < 1) {
+//         ev.preventDefault();  // stop scrolling in another direction
+//         timeline.scrollLeft += (ev.deltaY + ev.deltaX);
+//     } else {
+//
+//     }
+//
+//     //console.log(calc)
+//
+// });
+
+// Drag to scroll
+// =======================================
+const slider = document.querySelector('#timeline');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 3; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+    console.log(walk);
+});
+
+
 
